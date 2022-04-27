963be7f7d9f9f35648fba967a7b5a6516614485b
Artillis <aadejarde15@gmail.com>
2022-04-27 10:51:17 -0300

    Novas implementações, e atualizações dos gráficos
    - inclusão do dataset:
    curso período
    novas gráficos, e atualizações dos eixos dos gráficos.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from matplotlib.axis import YAxis
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -5,25 +6,40 @@ import csv
 import plotly.graph_objects as go
 import plotly.express as px
 
-
 st.write('''# Análise dos Cursos de BSI - UAST
 - Análise de Situação Unidade Curricular por Período''')
 df = pd.read_csv('df_vinculo_disciplina_periodo.csv')
 df_selection = df.query("PERIODO >= 2000.1 & CURSO == 'BACHARELADO EM SISTEMAS DE INFORMAÇÃO - UAST'")
-#FILTRO
-curso_filtro = st.sidebar.selectbox(
-    'CURSO',
-    options = df_selection[str('CURSO')].unique(),
-)
-periodo_filtro = st.sidebar.selectbox(
-    'PERÍODO',
-    options = df_selection[str('PERIODO')].unique(),
-)
-unidade_filtro = st.sidebar.selectbox(
-    'UNIDADE CURRICULAR',
-    options = df_selection[str('UNIDADE_CURRICULAR')].unique(),
-)
 
+df_curso = pd.read_csv('df_vinculo_curso_periodo.csv')
+df_selection_curso = df_curso.query("CURSO == 'BACHARELADO EM SISTEMAS DE INFORMAÇÃO - UAST' |  CURSO == 'BACHARELADO EM SISTEMAS DE INFORMAÇÃO' | CURSO == 'BACHARELADO EM SISTEMAS DE INFORMAÇÃO - UEDT'") #& CURSO == 'BACHARELADO EM SISTEMAS DE INFORMAÇÃO - UAST'
+#CURSO == 'BACHARELADO EM SISTEMAS DE INFORMAÇÃO - UAST' &  CURSO == 'BACHARELADO EM SISTEMAS DE INFORMAÇÃO' & CURSO == 'BACHARELADO EM SISTEMAS DE INFORMAÇÃO - UEDT'
+
+
+#FILTRO
+st.sidebar.title("Filtros")
+
+lista_curso = df_selection_curso[str('CURSO')].unique().tolist()
+with st.sidebar.expander(label='Cursos', expanded=True): 
+    for i, todo_text in enumerate(lista_curso):
+        periodo_filtro = st.checkbox(label=f"{todo_text}",key=i)
+
+lista_disciplinas = df_selection[str('UNIDADE_CURRICULAR')].unique().tolist()
+with st.sidebar.expander(label='Disciplinas', expanded=True): 
+    for i, todo_text in enumerate(lista_disciplinas):
+        periodo_filtro = st.checkbox(label=f"{todo_text}",key=i)
+
+lista_periodo = df_selection_curso[str('PERIODO')].unique().tolist()
+with st.sidebar.expander(label='Período', expanded=True): 
+    for i, todo_text in enumerate(lista_periodo):
+        periodo_filtro = st.checkbox(label=f"{todo_text}",key=i)
+
+lista_situacao = df_selection_curso[str('SITUACAO')].unique().tolist()
+with st.sidebar.expander(label='Situação', expanded=True): 
+    for i, todo_text in enumerate(lista_situacao):
+        periodo_filtro = st.checkbox(label=f"{todo_text}",key=i)
+
+#Página inicial
 
 df_situacao_tipo_unidade = df_selection.query('TIPO_UNIDADE_CURRICULAR == "ESTAGIO" | TIPO_UNIDADE_CURRICULAR == "TCC" | TIPO_UNIDADE_CURRICULAR == "MONOGRAFIA" | SITUACAO == "APROVADO" | SITUACAO == "REPROVADO" ')
 df_situacao_tipo_unidade = pd.DataFrame(df_situacao_tipo_unidade.groupby(['SITUACAO','PERIODO']).count()['TIPO_UNIDADE_CURRICULAR']).reset_index()
@@ -45,13 +61,14 @@ qtd_reprovado = pd.DataFrame(df_situacao_tipo_unidade.groupby(['SITUACAO_REPROVA
 qtd_cancelado = pd.DataFrame(df_situacao_tipo_unidade.groupby(['SITUACAO_CANCELADO']).agg({'TIPO_UNIDADE_CURRICULAR' : np.sum}))
 qtd_dispensado = pd.DataFrame(df_situacao_tipo_unidade.groupby(['SITUACAO_DISPENSADO']).agg({'TIPO_UNIDADE_CURRICULAR' : np.sum}))
 
-
 st.write(
-".  \n  Nº Aprovados: ", qtd_aprovado.iat[0,0], 
-".  \n  Nº Reprovados: ",qtd_reprovado.iat[0,0],
-".")
+"Nº Aprovados: ", qtd_aprovado.iat[0,0], 
+" \n  Nº Reprovados: ",qtd_reprovado.iat[0,0],
+"")
 
 # Grafico 1 - Situação Unidade Curricular por Período
+st.write('- Análise de Situação dos estudantes aprovados e reprovados')
+col1, col2 = st.columns(2)
 df_unidade_situacao = pd.DataFrame(df_selection.groupby(['SITUACAO','PERIODO']).count()['UNIDADE_CURRICULAR']).reset_index()
 
 df_aprovado1= df_unidade_situacao.query('SITUACAO=="REPROVADO"')
@@ -71,14 +88,15 @@ chart_1 = px.bar( df_unidade_situacao, x = 'PERIODO', y = 'UNIDADE_CURRICULAR', 
 })
 
 chart_1.update_layout(xaxis_type='category', yaxis_title='Quantidade de estudantes')
-chart_1.update_xaxes(categoryorder='category ascending', tickangle= 45)
+chart_1.update_xaxes(categoryorder='category ascending', tickangle= 45, showline=True, showgrid=False)
+chart_1.update_yaxes(showline=True, showgrid=False)
 chart_1.update_layout({
     'plot_bgcolor': 'rgba(0, 0, 0, 0)',   
 })
-st.write(chart_1)
+col1.plotly_chart(chart_1, use_container_width = True)
+#st.write(chart_1)
 
 # Grafico 2 - Situação dos estudantes aprovados e reprovados
-st.write('- Análise de Situação dos estudantes aprovados e reprovados')
 
 chart_3= px.bar( df_situacao_tipo_unidade, x = 'PERIODO', y = 'TIPO_UNIDADE_CURRICULAR', color = 'SITUACAO', barmode = 'group',labels={
 'PERIODO' : ' Período ',
@@ -93,9 +111,13 @@ chart_3= px.bar( df_situacao_tipo_unidade, x = 'PERIODO', y = 'TIPO_UNIDADE_CURR
     "OUTRO" : "#5a5a5a"
 })
 chart_3.update_layout(xaxis_type='category', yaxis_title='Situação em estágio/TCC/Monografia')
-chart_3.update_xaxes(categoryorder='category ascending', tickangle= 45)
+chart_3.update_xaxes(categoryorder='category ascending', tickangle= 45, showline=True, showgrid=False)
+chart_3.update_yaxes(showline=True, showgrid=False)
 chart_3.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)',})
-st.plotly_chart(chart_3)
+col2.plotly_chart(chart_3, use_container_width = True)
+#st.plotly_chart(chart_3)
+
+
 
 # Gráfico de pizza 1
 st.write('- Análise de Situação dos estudantes em gráfico de pizza')
@@ -128,3 +150,30 @@ st.write('- Análise de disciplinas em Gráfico de Pizza')
 df_unidade_s = pd.DataFrame(df_selection.groupby(['UNIDADE_CURRICULAR','PERIODO']).count()['SITUACAO_SIGA']).reset_index()
 fig_PIE = px.pie(df_unidade_s, values='SITUACAO_SIGA', names='UNIDADE_CURRICULAR')
 st.write(fig_PIE)
+
+bar_3 = px.histogram(df_selection, x="SITUACAO", y="MEDIA", color="NOME_CAMPUS", barmode='group', text_auto=True, height=400)
+bar_3.update_layout(xaxis_title='Situação do Estudante', yaxis_title='Quantidade de Vinculados')
+bar_3.update_xaxes(showline=True, showgrid=False)
+bar_3.update_yaxes(showline=True, showgrid=False)
+st.write(bar_3)
+#gráfico bar
+df_situacao = df_selection_curso
+
+bar = px.histogram(df_situacao, x='SITUACAO', y='QTD_VINC',
+             color='CAMPUS', barmode='group', text_auto=True)
+bar.update_layout(xaxis_title='Situação do Estudante', yaxis_title='Quantidade de Vinculados no Campus')
+bar.update_xaxes(showline=True, showgrid=False)
+bar.update_yaxes(showline=True, showgrid=False)
+st.write(bar)
+
+bar_2 = px.histogram(df_situacao, x='SITUACAO', y='QTD_VINC',
+             color='CURSO', barmode='group', text_auto=True)
+bar_2.update_layout(xaxis_title='Situação do Estudante', yaxis_title='Quantidade de Vinculados')
+bar_2.update_xaxes(showline=True, showgrid=False)
+bar_2.update_yaxes(showline=True, showgrid=False)
+st.write(bar_2)
+
+fig4 = px.treemap(df_situacao, path=[px.Constant("TODOS"), 'SITUACAO'],
+                    values='QTD_VINC'
+)
+st.write(fig4)
